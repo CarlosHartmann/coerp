4fdc1ad3dd9aeee0600824b2df74bd2c7eb76498
Smogshaik <github@smog.sh>
2023-08-23 22:25:17 +0200

    filterin, I think

--- coerp/conf_score_filtering.py
+++ coerp/conf_score_filtering.py
@@ -1,9 +1,9 @@
 import csv
 
 with open("/Users/uni/coerp/output/normalizations_w_latin_conf-scores.csv", "r", encoding="utf-8") as infile:
-    with open("/Users/uni/coerp/output/normalizations_w_latin_conf-scores_over-3.csv", "w", encoding="utf-8") as outfile:
+    with open("/Users/uni/coerp/output/normalizations_w_latin_conf-scores_over-50%.csv", "w", encoding="utf-8") as outfile:
         reader = csv.reader(infile, delimiter=';', quotechar='"', quoting = csv.QUOTE_MINIMAL)
         writer = csv.writer(outfile, delimiter=';', quotechar='"', quoting = csv.QUOTE_MINIMAL)
         for line in reader:
-            if float(line[-1]) > 0.4:
+            if float(line[-1]) > 0.5:
                 writer.writerow(line)
