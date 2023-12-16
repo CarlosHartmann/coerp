b9a3051ba10066a58f0e23d9d26504c8192dce6c
Smogshaik <github@smog.sh>
2023-12-16 14:56:45 +0100

    Commented and cleaned up first stable version

--- coerp/export.py
+++ coerp/export.py
@@ -9,13 +9,22 @@ from lxml import etree as ET
 
 testfile = "/Users/chartman/Documents/GitHub/coerp/test/bio_1510_More_R3"
 
-ns = '{http://www.tei-c.org/ns/1.0}'
+ns = '{http://www.tei-c.org/ns/1.0}' #used in all TEI documents (which is all of the COERP documents)
 
 def is_only_whitespace(text):
-    return True if '   ' in text else False
+    '''
+    Removes chunks of only whitespace, ideally without catching legitimate cases like single spaces, linebreaks, or tabs.
+    Initially, only chunks of the shape '\\n           ' were caught. For maximum precision, only this pattern is caught here.
+    The function extract_text sometimes still catches blocks of whitespace meant for XML formatting that this function hopefully filters out.
+    '''
+    return True if re.search('^\n\s{4,}', text) else False
 
 
 def get_unique_child_element_names(element, unique_elements=set()):
+    '''
+    Returns a list of unique elements found in a document.
+    Used for scouting the documents before script development.
+    '''
     # Add the name of the current element to the set
     unique_elements.add(element.tag)
 
@@ -26,11 +35,17 @@ def get_unique_child_element_names(element, unique_elements=set()):
     return unique_elements
 
 
-def is_under_skippable_tag(element):
-    skippable_tags = [f'{ns}sic', f'{ns}fw']
+def is_under_skippable_tag(element, skippables = [f'{ns}sic', f'{ns}fw']):
+    '''
+    Identifies if any given XML element is somewhere inside a tag I want to skip entirely.
+    This is necessary because the extract_text() function is recursive so child elements of skipped elements will still get processed.
+    Hence, this function always checks all ancestors until root to check for the to-be-skipped ones.
+    Currently, this is not necessary as the script works as expected anyway.
+    '''
+    
     parent = element.getparent()
     while parent is not None:
-        if parent.tag in skippable_tags:
+        if parent.tag in skippables:
             return True
         parent = parent.getparent()
     return False
@@ -38,12 +53,12 @@ def is_under_skippable_tag(element):
 
 def extract_text(xml_file):
     # Parse the XML file
-    parser = ET.XMLParser(remove_blank_text=False) # putting this to True would make things easier, but it would also introduce unfixable bugs in the export
+    parser = ET.XMLParser(remove_blank_text=False) # setting remove_blank_text to True would make things easier, but it would also introduce unfixable bugs in the export in the form of erroneously removed spaces between words
     tree = ET.parse(xml_file, parser)
     root = tree.getroot()
 
-    # Focus on the specific part of the document
-    text_body = root[1][0]  # Adjust this index based on the structure of your XML
+    # Skip teiHeader and immediately access the body element (I believe always inside a <text> element)
+    text_body = root[1][0]
 
     # Initialize an empty list to hold the text content
     text_content = []
@@ -51,28 +66,34 @@ def extract_text(xml_file):
     # Define a function to recursively extract text
     def extract_text_recursive(element):
         if element.tag in [f'{ns}head', f'{ns}p', f'{ns}corr', f'{ns}quote', f'{ns}choice', f'{ns}join']:
+            # these mark the border between different texts within a document, marking by adding additional linebreak – might need to be handled differently at a later stage
             if element.tag in [f'{ns}p', f'{ns}div', f'{ns}head']:
                 text_content.append('\n')
 
-            if element.text and not is_only_whitespace(element.text):
+            if element.text and not is_only_whitespace(element.text) and element.tag == f'{ns}head': # Just as a help for myself – titles might need to be handled differently at a later stage
+                text_content.append(f'Title: {element.text}')
+
+            elif element.text and not is_only_whitespace(element.text):
                 text_content.append(element.text)
 
-            for child in element:
+            for child in element: # this is mainly to catch all the elements within paragraphs such as linebreaks and simple normalisations
                 if child.tag in [f'{ns}normalised', f'{ns}hi', f'{ns}notvariant']:
                     if child.text:
-                        text_content.append(child.text.lstrip())
+                        text_content.append(child.text)
 
-                elif child.tag == f'{ns}lb' and not text_content[-1] == '\n':
+                elif child.tag == f'{ns}lb' and not text_content[-1] == '\n': # reduces unnecessary(?) chains of several linebreaks to just one at a time.
                     text_content.append('\n')
 
-                elif child.tag not in [f'{ns}sic', f'{ns}fw']:
+                elif child.tag not in [f'{ns}sic', f'{ns}fw']: # sic always occurs before the corr element which contains what we actually want for the final corpus
                     extract_text_recursive(child)
 
-                if child.tail and not is_only_whitespace(child.tail):
+                if child.tail and not is_only_whitespace(child.tail): # the tails are most commonly text or simple spaces, but sometimes also chunks of formatting-whitespace
                     text_content.append(child.tail.replace('\n', ''))
 
     # Iterate through the elements in a linear fashion within the specified part of the document
     for elem in text_body.iter():
+        # the content of certain elements was doubled at the end of paragraphs without this line
+        # why this fixed it and not filtering within the extract_text_recursive() function, I still do not know
         if elem.tag not in [f'{ns}normalised', f'{ns}corr', f'{ns}choice', f'{ns}join', f'{ns}sic']:
             extract_text_recursive(elem)
 
