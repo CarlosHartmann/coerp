b64d1378c116f5d851f8da985f5b1656d08baa65
Smogshaik <github@smog.sh>
2024-01-18 15:51:34 +0100

    Fixed repetition bug
    Needs in-depth probing of output corpus

--- coerp/export.py
+++ coerp/export.py
@@ -56,8 +56,9 @@ def extract_text(xml_file):
     # Skip teiHeader and immediately access the body element (I believe always inside a <text> element)
     text_body = root[1][0]
 
-    # Initialize an empty list to hold the text content
+    # Initialize an empty list to hold the text content and another to keep track of handled elements
     text_content = []
+    handled = []
 
     # Define a function to recursively extract text
     def extract_text_recursive(element):
@@ -68,45 +69,65 @@ def extract_text(xml_file):
                     text_content.append('\n')
             if element.tag == f'{ns}head': # adding a double linebreak to highlight a new incoming title
                 if len(text_content) > 0:
-                    text_content.append('\n\n')
-
+                    text_content.append('\n\n') 
 
             if element.text and not is_only_whitespace(element.text) and element.tag == f'{ns}head': # Just as a help for myself – titles might need to be handled differently at a later stage
-                text_content.append(f'Title: {element.text}')
+                if element not in handled:
+                    text_content.append(f'Title: {element.text}')
+                    handled.append(element)
 
             elif element.text and not is_only_whitespace(element.text):
-                text_content.append(remove_huge_spaces(element.text))
+                if element not in handled:
+                    text_content.append(remove_huge_spaces(element.text))
+                    handled.append(element)
 
             for child in element: # this is mainly to catch all the elements within paragraphs such as linebreaks and simple normalisations
+
+                tail_id = f'{child} – tail' if child.tag != f'{ns}lb' else f'{child.tag} at {child.sourceline}'
+
                 if child.tag in [f'{ns}hi', f'{ns}notvariant']:
-                    if child.text:
+                    if child.text and child not in handled:
                         text_content.append(child.text)
+                        handled.append(child)
+
                 elif child.tag == f'{ns}normalised':
                     if child.attrib['auto'] == 'true':
                         original = child.attrib['orig']
                         confidence_value = detector.compute_language_confidence(original, Language.LATIN)
-                        if confidence_value > 0.965:
+                        if confidence_value > 0.965 and child not in handled:
                             text_content.append(original)
-                        else:
+                            handled.append(child)
+                        elif confidence_value < 0.965 and child not in handled:
                             text_content.append(child.text)
+                            handled.append(child)
 
-                    elif child.attrib['auto'] == 'false':
+                    elif child.attrib['auto'] == 'false' and child not in handled:
                         text_content.append(child.text)
+                        handled.append(child)
 
-                elif child.tag not in [f'{ns}sic', f'{ns}fw', f'{ns}l', f'{ns}p', f'{ns}quote']: # sic always occurs before the corr element which contains what we actually want for the final corpus | similar problem with <l> which is used in <sp>
+                elif child.tag not in [f'{ns}sic', f'{ns}fw', f'{ns}l', f'{ns}p'] and child not in handled and tail_id not in handled: # sic always occurs before the corr element which contains what we actually want for the final corpus | similar problem with <l> which is used in <sp>
                     extract_text_recursive(child)
 
-                if child.tail and not is_only_whitespace(child.tail): # the tails are most commonly text or simple spaces, but sometimes also chunks of formatting-whitespace
+                if child.tail and not is_only_whitespace(child.tail) and tail_id not in handled: # the tails are most commonly text or simple spaces, but sometimes also chunks of formatting-whitespace
                     if child.tag in [f'{ns}lb', f'{ns}p', f'{ns}l'] and child.tail.startswith('\n    '): # trying to catch those formatting chunks of whitespace right after a linebreak
-                        text_content.append(child.tail.strip())
+                        text_content.append(re.sub('\n +', '\n',child.tail))
+                        handled.append(tail_id)
+
                     elif re.search(' *\n {4,}', child.tail):
                         tail = re.sub('^ *\n','\n', child.tail)
                         tail = re.sub('\n {4,}', '\n', tail)
                         text_content.append(tail)
+                        handled.append(tail_id)
+                        
+
                     elif child.tag.startswith('\n    '):    
                         text_content.append(child.tail.replace('\n', ''))
+                        handled.append(tail_id)
+
                     else:
                         text_content.append(child.tail)
+                        handled.append(tail_id)
+                        
 
     # Iterate through the elements in a linear fashion within the specified part of the document
     for elem in text_body.iter():
